7a6a090340dac7f061427dc9f19dabd7304826e8
Anthony Lisa <Anthony.Lisa0@gmail.com>
2019-07-18 15:13:23 -0700

    updated to include whitelist

--- discord api.py
+++ discord api.py
@@ -1,33 +1,34 @@
 import discord
 import re
+import tokens
 
-TOKEN = 'NTIxODQxODI4OTk2MzE3MTg0.DvCS8Q.zXwIvwsKP-vWl-M5K5SoF_KGh5k'
+TOKEN = tokens.getToken()
 
 client = discord.Client()
+replace_with = '!@#$%^&*()\':;.,><UwU--_--=+`~\|][}{[/?'
 
 def vulgar(msg):
-    replace_with = '!@#$%^&*()\':;.,><UwU--_--=+`~\|][}{[/?'
-
     bad = False
-
-    banned = """anal,anus,arse,ass,ballsack,balls,bastard,
-    bitch,biatch,bloody,blowjob,blow job,bollock,bollok,
-    boner,boob,bugger,bum,butt,buttplug,clitoris,cock,
-    coon,crap,cunt,damn,dick,dildo,dyke,fag,faggot,faggy,feck,fellate,
-    fellatio,felching,fuck,f u c k,fudgepacker,fudge packer,
-    flange,Goddamn,God damn,hell,homo,jerk,jizz,knobend,knob end,
-    labia,lmao,lmfao,muff,nigger,nigga,penis,piss,poop,prick,
-    pube,pussy,queer,scrotum,sex,shit,s hit,sh1t,slut,smegma,spunk,
-    tit,tosser,turd,twat,vagina,wank,whore"""
-    banned = banned.split(',')
     clean_msg = None 
-    temp_msg = msg.split(" ")
-
+    temp_msg = re.sub(r"[^a-zA-Z0-9]+", '', msg.lower())
+    
     for i in banned:
-        if i in msg:
-            pattern = r"\b" + re.escape(i) + r"\b"
-            clean_msg = re.sub(pattern, replace_with[:len(i)-1],msg)
-            bad = True
+        if i in temp_msg:
+            if bad:
+                temp_msg = clean_msg
+            start = temp_msg.find(i)
+            p_start = min(temp_msg[:start].find(" "),start)
+            if p_start != -1:
+                start = p_start
+            else: 
+                start =0
+                
+            end = max(temp_msg[start:].find(" "),len(temp_msg))
+            potential = temp_msg[start:end]
+
+            if potential.strip() not in white_list:
+                clean_msg = re.sub(re.escape(i), replace_with[:len(i)-1],temp_msg)
+                bad = True
 
 
 
@@ -53,17 +54,25 @@ async def on_message(message):
         await client.delete_message(message)
         cleaned_msg = 'Watch your mouth! {0.author.mention}:\n <{1}>'.format(message,cleaned_msg)
         await client.send_message(message.channel, cleaned_msg)
-    else:
-        await client.send_message(message.channel, "No")
+    
         
 
 
-
+banned=set()
+white_list = set()
 @client.event
 async def on_ready():
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
     print('------')
+    
+    with open('BadWords.txt','r') as bad_words:
+        for line in bad_words:
+            banned.add(line.strip())
+    with open('whitelist.txt','r') as white:
+        for line in white:
+            white_list.add(line.strip().lower())
+
 
 client.run(TOKEN)
